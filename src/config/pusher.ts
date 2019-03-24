17dd8a5fea5799b16824e231a14226f563e75596
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-03-24 21:50:37 +0200

    initial commit + pusher config

---
+++ src/config/pusher.ts
@@ -0,0 +1,9 @@
+import Pusher = require('pusher');
+
+export default new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: 'eu',
+  useTLS: true
+});
