fed46e73f3018297279a778c0f90a0d29f344dc2
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-05-11 13:12:59 +0300

    updated docker-compose

--- src/models/rating.js
+++ src/models/rating.js
@@ -2,8 +2,8 @@ const bookshelf = require('../config/bookshelf');
 const Fields = require('bookshelf-schema/lib/fields');
 const Relations = require('bookshelf-schema/lib/relations');
 
-module.exports = bookshelf.model('Question', {
-  tableName: 'questions',
+module.exports = bookshelf.model('Rating', {
+  tableName: 'ratings',
   uuid: true,
   schema: [Fields.StringField('userId'), Relations.BelongsTo('Question')]
 });
